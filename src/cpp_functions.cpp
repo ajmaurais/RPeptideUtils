3378b93a52f659f8e4494c2e2df9a6b805e5e527
ajmaurais <ajm.services@protonmail.com>
2026-02-17 17:07:37 -0500

    Remove trailing white space from cpp_functions.cpp

--- src/cpp_functions.cpp
+++ src/cpp_functions.cpp
@@ -30,7 +30,7 @@ std::string _getPackageData(std::string filename, std::string packageName = "RPe
 }
 
 //' Get indices of fragment ions for peptide sequence.
-//' 
+//'
 //' @title Get fragment ion sequence indices.
 //' @param seq Peptide sequence as a string.
 //' @return fragments IntegerMatrix where each row is a fragment ion. The first column is the startig index of the fragment ion in the peptide sequence and the second column is the fragment length.
@@ -55,7 +55,7 @@ Rcpp::IntegerMatrix getFragmentIonIndices(std::string seq)
 }
 
 //' Get sequences of fragment ions for peptide sequence.
-//' 
+//'
 //' @title Get fragment ion sequences.
 //' @param seq Peptide sequence as a string.
 //' @return fragments Named CharacterVector where the names are the ions and the values are the sequences.
@@ -75,46 +75,46 @@ Rcpp::CharacterVector getFragmentIonSequences(std::string seq)
 }
 
 //' Get protein sequences for a vector of uniprot IDs
-//' 
+//'
 //' @title Get protein IDs in fasta file for a vector of Uniprot IDs
 //' @param ids CharacterVector of uniprot IDs
-//' @param fastaPath path to fasta formatted file to look up protein sequences. 
+//' @param fastaPath path to fasta formatted file to look up protein sequences.
 //' @return CharacterVector of protein sequences in same order as ids.
-//' 
+//'
 //' @examples
 //' #By default the fasta file included in the package containing human protein sequences is used.
 //' getSequences(c("A0MZ66", "A6NMY6", "O00213", "O00213"))
-//' 
+//'
 //' #A fasta file can also be manually specified.
 //' fasta_path <- system.file('extdata/Human_uniprot-reviewed_20171020.fasta', package = 'RPeptideUtils')
 //' getSequences(c("A0MZ66", "A6NMY6", "O00213", "O00213"), fasta_path)
-//' 
+//'
 // [[Rcpp::export]]
 Rcpp::CharacterVector getSequences(const Rcpp::CharacterVector& ids, std::string fastaPath = "")
 {
     std::string _fastaPath = fastaPath.empty() ? _getPackageData("extdata/Human_uniprot-reviewed_20171020.fasta") : fastaPath;
-    
+
     Rcpp::CharacterVector ret;
-    
+
     utils::FastaFile fasta(true, utils::absPath(_fastaPath));
     if(!fasta.read()) throw std::runtime_error("Could not read fasta file!");
-    
+
     size_t len = ids.size();
     for(size_t i = 0; i < len; i++){
         ret.push_back(fasta.getSequence(std::string(ids[i])));
     }
-    
+
     return ret;
 }
 
 //' Get n residues before query in ref. If n overruns ref, the maximum number of characters will be returned.
-//' 
+//'
 //' @title get n residues before query.
 //' @param query String to search for.
 //' @param ref String to search in.
 //' @param n Number of residues in output.
 //' @param noExcept Should an std::runtime_error be thrown if query is not in ref?
-//' 
+//'
 //' @return n residues before query.
 //'
 // [[Rcpp::export]]
@@ -138,13 +138,13 @@ Rcpp::CharacterVector nBefore(const Rcpp::CharacterVector& query, const Rcpp::Ch
 }
 
 //' Get n residues after query in ref. If n overruns ref, the maximum number of characters will be returned.
-//' 
+//'
 //' @title get n residues after query.
 //' @param query String to search for.
 //' @param ref String to search in.
 //' @param n Number of residues in output.
 //' @param noExcept Should an std::runtime_error be thrown if query is not in ref?
-//' 
+//'
 //' @return n residues after query.
 //'
 // [[Rcpp::export]]
@@ -168,13 +168,13 @@ Rcpp::CharacterVector nAfter(const Rcpp::CharacterVector& query, const Rcpp::Cha
 }
 
 //' Get the index of residue n of query in ref. If n is -1, the index of the last residue in query is returned.
-//' 
+//'
 //' @title get the index of nth residues of query in ref.
 //' @param query String to search for.
 //' @param ref String to search in.
 //' @param n Residue number in query.
 //' @param noExcept Should an std::runtime_error be thrown if query is not in ref?
-//' 
+//'
 //' @return n residues after query.
 //'
 // [[Rcpp::export]]
@@ -216,17 +216,17 @@ std::string makeSequenceFromFullSequence(std::string fs)
 //'
 //' @examples
 //' getModifiedResidues(c("Q00839", "Q9HCS7", "Q7L014"), c("APQC*LGK", "FADMEC*K", "GAEIIVC*TPGR"))
-//' 
+//'
 // [[Rcpp::export]]
 Rcpp::CharacterVector getModifiedResidues(const Rcpp::CharacterVector& ids,
     const Rcpp::CharacterVector& peptideSeq, std::string fastaPath = "", std::string modSep = "|")
 {
     std::string _fastaPath = fastaPath.empty() ? _getPackageData("extdata/Human_uniprot-reviewed_20171020.fasta") : fastaPath;
-    
+
     size_t len = ids.size();
     if(len != peptideSeq.size())
         throw std::runtime_error("ids.size() != peptideSeq.size()");
-    
+
     //init FastaFile
     utils::FastaFile fasta(true, _fastaPath);
         if(!fasta.read()) throw std::runtime_error("Could not read fasta file!");
@@ -246,20 +246,20 @@ Rcpp::CharacterVector getModifiedResidues(const Rcpp::CharacterVector& ids,
 }
 
 //' Combine concated mods from multiple peptides into a single string.
-//' 
+//'
 //' @title Combine mods from multiple peptides into a single string
 //' @param mods Modifications to combine
 //' @param sep delimiter separating modifications
 //' @return Modifications combined into a single string
-//' 
+//'
 //' @examples
 //' combineMods(c('C157', 'C157|C125', 'C50', 'C125'))
-//' 
+//'
 // [[Rcpp::export]]
 std::string combineMods(const Rcpp::CharacterVector& mods, char sep = '|')
 {
     std::set<std::string> found;
-    
+
     size_t len = mods.size();
     for(size_t i = 0; i < len; i++)
     {
@@ -267,16 +267,16 @@ std::string combineMods(const Rcpp::CharacterVector& mods, char sep = '|')
         utils::split(std::string(mods[i]), sep, temp);
         found.insert(temp.begin(), temp.end());
     }
-    
+
     return utils::concat(found.begin(), found.end());
 }
 
 //' Calculate peptide monoisotopic or average masses.
-//' 
+//'
 //' @title Calculate peptide masses
 //' @param sequences Peptide sequences
 //' @param monoMass Should monoisotopic mass be calculated. If false, average mass is calculated.
-//' @param residueAtoms Path to residueAtoms file. If blank, the default file included in the package is used. 
+//' @param residueAtoms Path to residueAtoms file. If blank, the default file included in the package is used.
 //' @return vector of peptide masses.
 //'
 //' @examples
@@ -290,26 +290,26 @@ Rcpp::NumericVector calcMass(const Rcpp::StringVector& sequences,
     //get data file paths
     std::string residueAtomsPath = residueAtoms.empty() ? _getPackageData("defaultResidueAtoms.txt") : residueAtoms;
     char avg_mono = monoMass ? 'm' : 'a';
-    
+
     //init residues
     utils::Residues residues(residueAtomsPath);
     if(!residues.initialize(false)) throw std::runtime_error("Error reading required files for calcMass!");
-    
+
     size_t len = sequences.size();
     Rcpp::NumericVector ret(len);
     for(size_t i = 0; i < len; i++){
         ret[i] = residues.calcMass(std::string(sequences[i]), avg_mono);
     }
-    
+
     return ret;
 }
 
 //' Calculate peptide molecular formulas
-//' 
+//'
 //' @title Calculate peptide formulas
 //' @param sequences Peptide sequences
 //' @param subscripts Should formulas have subscripts or normal baseline numbers?
-//' @param residueAtoms Path to residueAtoms file. If blank, the default file included in the package is used. 
+//' @param residueAtoms Path to residueAtoms file. If blank, the default file included in the package is used.
 //' @return vector of peptide formulas.
 //'
 //' @examples
@@ -322,23 +322,23 @@ Rcpp::StringVector calcFormula(const Rcpp::StringVector& sequences,
 {
     //get data file paths
     std::string residueAtomsPath = residueAtoms.empty() ? _getPackageData("defaultResidueAtoms.txt") : residueAtoms;
-    
+
     //init residues
     utils::Residues residues;
     if(!residues.initialize(residueAtomsPath))
         throw std::runtime_error("Error reading required files for calcFormula!");
-    
+
     size_t len = sequences.size();
     Rcpp::StringVector ret(len);
     for(size_t i = 0; i < len; i++){
         ret[i] = residues.calcFormula(std::string(sequences[i]), subscripts);
     }
-    
+
     return ret;
 }
 
 //' Convert from 1 letter amino acid codes to 3
-//' 
+//'
 //' @title Convert to 3 letter amino acid codes
 //' @param sequences vector of sequences
 //' @param sep_in deliminator between amino acids in input
@@ -346,10 +346,10 @@ Rcpp::StringVector calcFormula(const Rcpp::StringVector& sequences,
 //' @param n_term_out string to append to n terminus
 //' @param c_term_out string to append to c terminus
 //' @return StringVector of peptides with three letter amino acid codes
-//' 
+//'
 //' @examples
 //' oneLetterToThree(c("AC*LLPETVNMEEYPYDAEY", "ALCAEFK", "AQUPIVER", "C*TGGEVGATSALAPK"))
-//' 
+//'
 // [[Rcpp::export]]
 Rcpp::StringVector oneLetterToThree(Rcpp::StringVector sequences,
                                     std::string sep_in = "",
@@ -368,7 +368,7 @@ Rcpp::StringVector oneLetterToThree(Rcpp::StringVector sequences,
 }
 
 //' Convert from 3 letter amino acid codes to 1
-//' 
+//'
 //' @title Convert to 1 letter amino acid codes
 //' @param sequences vector of sequences
 //' @param sep_in deliminator between amino acids in input
@@ -376,10 +376,10 @@ Rcpp::StringVector oneLetterToThree(Rcpp::StringVector sequences,
 //' @param n_term_out string to append to n terminus
 //' @param c_term_out string to append to c terminus
 //' @return StringVector of peptides with one letter amino acid codes
-//' 
+//'
 //' @examples
 //' threeLetterToOne(c("Ala-Cys*-Leu-Leu-Pro", "Ala-Leu-Cys-Ala", "Ala-Gln-Sec-Ile"), sep_in = "-")
-//' 
+//'
 // [[Rcpp::export]]
 Rcpp::StringVector threeLetterToOne(Rcpp::StringVector sequences,
                                     std::string sep_in = "",
@@ -437,12 +437,12 @@ Rcpp::DataFrame readFasta(std::string fastaPath = "", long n_entries = 0)
 }
 
 //' Get metadata about a fasta file.
-//' 
+//'
 //' @title Get fasta file info.
-//' 
+//'
 //' @param fastaPath Path to fasta file. Be default, fasta file included in package is used.
 //' @return List with slots for sequence count and vector of entry IDs contained in file.
-//' 
+//'
 // [[Rcpp::export]]
 Rcpp::List fastaInfo(std::string fastaPath = "")
 {
@@ -468,9 +468,9 @@ Rcpp::List fastaInfo(std::string fastaPath = "")
 //' quantifications. A row will be included in the output for each time an amino acid
 //' at a given position was included in a peptide in peptide_sequences. Additional processing
 //' is required to obtain summary values for each amino acid position.
-//' 
+//'
 //' @title Transpose peptide quantifications into amino acid level quantifications.
-//' 
+//'
 //' @param peptide_sequences List of peptide sequences.
 //' @param quantification Ratio or spectral count values for peptide_sequences.
 //' @param protein_seq Parent protein sequence.
@@ -514,24 +514,24 @@ Rcpp::DataFrame transpose_sequence(const Rcpp::StringVector& peptide_sequences,
 //' observable by MS. The m/z for peptides in charge states minCharge to maxCharge
 //' are calculated. If the m/z for any charge state is in between minMZ and maxMZ, the
 //' sequence will be appended to peptides.
-//' 
+//'
 //' @title Perform a virtual protease digest of a protein.
-//' 
+//'
 //' @param sequences StringVector containing protein sequences. Whitespace will automatically be removed.
 //' @param ids Names for the slot for each protein's peptides in output.
 //' @param nMissedCleavages number of missed cleavages to allow.
 //' @param cleavagePattern RegEx for protease cleavage pattern. The default is the pattern for trypsin.
 //' @param mz_filter Should peptides included in output be filtered by mz?
-//' @param residueAtoms Path to residueAtoms file. If blank, the default file included in the package is used. 
+//' @param residueAtoms Path to residueAtoms file. If blank, the default file included in the package is used.
 //' @param minMz Minimum m/z to allow in peptides.
 //' @param maxMz Maximum m/z to allow in peptides. Set to 0 for no upper bound on m/z.
 //' @param minCharge Minimum charge to consider when calculating m/z.
 //' @param maxCharge Maximum charge to consider when calculating m/z.
 //' @param minLen Minimum peptide length.
 //' @param maxLen Maximum peptide length. Set to 0 for no upper bound on length.
-//' 
+//'
 //' @return A list with named elements containing vectors of each input protein's peptides.
-//' 
+//'
 //' @examples
 //' digest(c("KLGAARKLGAGLAKVIGAGIGIGK", "KLGAARKLGAGLAKPVIGAGIGIGK"), c('a', 'b'))
 //'
@@ -636,18 +636,18 @@ void progressBarWorker(std::atomic<size_t>& index, size_t count,
 }
 
 //' Given a vector of peptide sequences, find all the proteins containing the peptide in a fasta file.
-//' 
+//'
 //' @title Find all proteins containing peptide sequences.
 //' @param peptides A character vector of peptide sequences.
-//' @param fastaPath path to fasta formatted file to look up protein sequences. 
+//' @param fastaPath path to fasta formatted file to look up protein sequences.
 //' @param progressBar Show progress bar?
 //' @param nThread Number of threads to use. By default use 1 thread per virtual core on machine.
 //' @return List where names are peptide sequences, and values are the ids for the matching proteins.
-//' 
+//'
 //' @examples
 //' fasta_path <- system.file('extdata/Human_uniprot-reviewed_20171020.fasta', package = 'RPeptideUtils')
 //' matchingProteins(c('PEPTIDE'), progressBar = FALSE, fastaPath = fasta_path)
-//' 
+//'
 // [[Rcpp::export]]
 Rcpp::List matchingProteins(Rcpp::CharacterVector peptides, std::string fastaPath = "",
                        bool progressBar = true, size_t nThread = 0)
@@ -721,7 +721,7 @@ Rcpp::List matchingProteins(Rcpp::CharacterVector peptides, std::string fastaPat
             ret.push_back(it.second, it.first);
         }
     }
-    if(progressBar){ 
+    if(progressBar){
         utils::printProgress(float(ret.size()) / float(len));
         Rcpp::Rcout << "\nDone!\n";
     }
@@ -734,11 +734,11 @@ Rcpp::List matchingProteins(Rcpp::CharacterVector peptides, std::string fastaPat
 //' @param cStrings The CharacterVector.
 //' @param verbose Print verbose output?
 //' @return CharacterVector with common begining and ending removed.
-//' 
+//'
 //' @examples
 //' smallestDifferentStrings(c('SHARED_START_first_SHARED_END',
 //'                            'SHARED_START_second_SHARED_END'))
-//' 
+//'
 // [[Rcpp::export]]
 Rcpp::CharacterVector smallestDifferentStrings(Rcpp::CharacterVector cStrings,
                                                  bool verbose = true)
