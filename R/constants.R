b667d002d447dce4f428b16dc56306e303197b26
ajmaurais <ajm.services@protonmail.com>
2026-03-05 15:36:47 -0500

    Add PROTON_MASS constant

--- R/constants.R
+++ R/constants.R
@@ -3,3 +3,11 @@
 #'
 #' @export
 H1_MASS <- 1.00782503207
+
+#' Mass of a proton (H+)
+#'
+#' Use this constant (not \code{H1_MASS}) when calculating m/z from neutral mass:
+#' \code{mz = (M + z * PROTON_MASS) / z}
+#'
+#' @export
+PROTON_MASS <- 1.00727646677
